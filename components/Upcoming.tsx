7fddf93086fa03a9d75ddefa374da8c1c4a507d1
kid-us <kidushh29@gmail.com>
2025-05-05 12:23:56 +0300

    Image size increased.

--- components/Upcoming.tsx
+++ components/Upcoming.tsx
@@ -1,5 +1,5 @@
 import { Link } from "expo-router";
-import { Image, Text, TouchableOpacity } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 interface Upcoming {
   id: number | string;
@@ -20,20 +20,23 @@ const Upcoming = ({
 }: Upcoming) => {
   return (
     <Link href={type === "movie" ? `/movies/${id}` : `/tvshows/${id}`} asChild>
-      <TouchableOpacity className="w-32 relative">
+      <TouchableOpacity className="w-40 relative">
         <Image
           source={{
             uri: poster
               ? `https://image.tmdb.org/t/p/w500${poster}`
               : `https://image.tmdb.org/t/p/w500${backdrop_path}`,
           }}
-          className="w-36 h-48 rounded-lg"
+          className="w-44 h-60 rounded-lg"
           resizeMode="cover"
         />
 
-        <Text className="text-sm mt-2 text-light-200" numberOfLines={1}>
-          {title ? title : name}
-        </Text>
+        {/* Title and Rate */}
+        <View>
+          <Text className="text-sm mt-2 text-light-200" numberOfLines={1}>
+            {title ? title : name}
+          </Text>
+        </View>
       </TouchableOpacity>
     </Link>
   );
