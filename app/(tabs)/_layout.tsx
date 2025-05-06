544f61dd76bb9df2c8b9d3f9689bd4229e16d54b
kid-us <kidushh29@gmail.com>
2025-05-06 18:00:57 +0300

    Tabs fixed.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,24 +1,7 @@
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { Tabs } from "expo-router";
 import React from "react";
-import { SafeAreaView, StatusBar, Text, View } from "react-native";
-
-const TabIcon = ({ focused, title, icon }: any) => {
-  if (focused) {
-    return (
-      <View className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded overflow-hidden bg-action">
-        <MaterialIcons name={icon} size={21} color={"#000"} />
-        <Text className="text-secondary font-bold ml-2 text-sm">{title}</Text>
-      </View>
-    );
-  }
-
-  return (
-    <View className="size-full justify-center items-center mt-4 rounded-full">
-      <MaterialIcons name={icon} size={21} color={"#A8B5DB"} />
-    </View>
-  );
-};
+import { SafeAreaView, StatusBar } from "react-native";
 
 const _layout = () => {
   return (
@@ -26,15 +9,14 @@ const _layout = () => {
       <StatusBar animated={true} backgroundColor={"#080708"} />
       <Tabs
         screenOptions={{
-          tabBarShowLabel: false,
           tabBarStyle: {
             backgroundColor: "#18181b",
-            height: 50,
             overflow: "hidden",
             borderWidth: 1,
             borderColor: "#0f0D23",
-            marginBottom: 50,
           },
+          tabBarActiveBackgroundColor: "#FFC20B",
+          tabBarActiveTintColor: "black",
         }}
       >
         <Tabs.Screen
@@ -42,8 +24,8 @@ const _layout = () => {
           options={{
             title: "Home",
             headerShown: false,
-            tabBarIcon: ({ focused }) => (
-              <TabIcon focused={focused} title="Home" icon={"home"} />
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="home" size={20} color={color} />
             ),
           }}
         />
@@ -53,8 +35,8 @@ const _layout = () => {
           options={{
             title: "Movies",
             headerShown: false,
-            tabBarIcon: ({ focused }) => (
-              <TabIcon focused={focused} title="Movies" icon={"movie-filter"} />
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="movie-filter" size={20} color={color} />
             ),
           }}
         />
@@ -64,8 +46,8 @@ const _layout = () => {
           options={{
             title: "Tv Shows",
             headerShown: false,
-            tabBarIcon: ({ focused }) => (
-              <TabIcon focused={focused} title="Tv Shows" icon={"live-tv"} />
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="live-tv" size={20} color={color} />
             ),
           }}
         />
@@ -75,8 +57,8 @@ const _layout = () => {
           options={{
             title: "Search",
             headerShown: false,
-            tabBarIcon: ({ focused }) => (
-              <TabIcon focused={focused} title="Search" icon={"search"} />
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="search" size={20} color={color} />
             ),
           }}
         />
--- components/MovieBanner.tsx
+++ components/MovieBanner.tsx
@@ -30,37 +30,27 @@ const MovieBanner = ({
             }}
             resizeMode="cover"
           >
-            <LinearGradient
-              colors={["rgba(0, 0, 0, 0.99)", "rgba(1, 1, 1, 0.30)"]}
-              start={{ x: 0.5, y: 1 }}
-              end={{ x: 0.5, y: 0 }}
-              style={{
-                position: "absolute",
-                top: 0,
-                bottom: 0,
-                left: 0,
-                right: 0,
-              }}
-            />
-            {/* Add any other overlay content here */}
-            <View className="flex justify-center items-start h-full px-5">
-              <View className="mt-64">
+            {/* Hero Title and overview */}
+            <View className="relative flex justify-center items-start h-full px-5">
+              <LinearGradient
+                colors={["rgba(0, 0, 0, 0.99)", "rgba(1, 1, 1, 0.30)"]}
+                start={{ x: 0.5, y: 1 }}
+                end={{ x: 0.5, y: 0 }}
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                }}
+              />
+              <View className="absolute bottom-5 mx-5">
                 <Text className="text-white text-3xl font-bold">
                   {title ? title : name}
                 </Text>
                 <Text className="text-zinc-400 mt-5" numberOfLines={5}>
                   {overview}
                 </Text>
-                {/* <Pressable
-                  className="mt-5 w-full px-16 py-2 rounded-lg bg-action"
-                  onPress={() => router.push(`/movies/${id}`)}
-                >
-                  <FontAwesome5
-                    name="long-arrow-alt-right"
-                    size={20}
-                    color={"#fff"}
-                  />
-                </Pressable> */}
               </View>
             </View>
           </ImageBackground>
